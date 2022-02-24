96c7faa2fffebc74a6a1de4b762090e461e847c8
nmn <nmngarg030798@gmail.com>
2022-02-24 15:01:42 +0530

    quiz-app-completed

--- quiz-app/code/index.js
+++ quiz-app/code/index.js
@@ -1,6 +1,12 @@
 // 
-
-let form = document.querySelector(`form`);
+let quizElm = document.querySelector(`.quiz`);
+let prevBtn = document.querySelector(`.prev-btn`);
+let nextBtn = document.querySelector(`.next-btn`);
+let resultBtn = document.querySelector(`.result-btn`);
+let ansBtn = document.querySelector(`.answer-btn`);
+let totalQues = document.querySelector(`.total-ques`);
+let fText = document.querySelector(`.f-text`);
+let resetBtn = document.querySelector(`.restart`);
 
 
 
@@ -9,12 +15,35 @@ class Quiz {
         this.allQuestions = allQuestions;
         this.activeQuesIndex = 0;
         this.score = score;
+        this.clickOnce = 0;
     }
     addQues(title, options, correctAnswer){
         let question = new Question(title, options, correctAnswer);
         this.allQuestions.push(question);
         // this.createUI();
         return this.allQuestions.length;
+    }
+    handleBtns(){
+        if(this.activeQuesIndex === 0){
+            prevBtn.style.visibility = "hidden";
+            resultBtn.style.visibility = "hidden";
+        }else if (this.activeQuesIndex === this.allQuestions.length - 1){
+            nextBtn.style.visibility = "hidden";
+            resultBtn.style.visibility = "visible";
+        }else{
+            prevBtn.style.visibility = "visible";
+            nextBtn.style.visibility = "visible";
+            resultBtn.style.visibility = "hidden";
+        }   
+    }
+    getAnswers(){
+        if(this.activeQuesIndex === 0){
+            ansBtn.style.display = "none";
+        }else if (this.activeQuesIndex === this.allQuestions.length - 1){
+            ansBtn.style.display = "inline-block";
+        }else{
+            ansBtn.style.display = "none";
+        }
     }
     nextQuestion(){
         this.activeQuesIndex = this.activeQuesIndex + 1;
@@ -25,10 +54,24 @@ class Quiz {
         this.createUI();
     }
     updateScore(){
-        
+        this.score  = this.score + 1;
+        return this.score;
+    }
+    restart(){
+        this.activeQuesIndex = 0;
+        // this.handleBtns();
+        fText.style.display = "none";
+        nextBtn.style.visibility = "visible";
+        ansBtn.style.display = "none";
+        this.createUI();
     }
     createUI(){
+        quizElm.innerHTML =  "";
         let activQuestion = this.allQuestions[this.activeQuesIndex];
+        this.clickOnce = 0;
+        this.handleBtns();
+        totalQues.innerText = `Total Questions: ${this.allQuestions.length}`;
+        let form = document.createElement(`form`);
         let fieldset = document.createElement(`fieldset`);
         fieldset.name = "fields";
         let legend = document.createElement(`legend`);
@@ -41,6 +84,11 @@ class Quiz {
         button.innerText = "Submit";
         divBtn.append(button);
 
+        ansBtn.addEventListener(`click`, () => {
+            fText.style.display = "inline-block";
+        })
+        fText.innerText = `correct Answer is: ${activQuestion.getCorrectAnswer()}`
+
         activQuestion.options.forEach((option, index) => {
             let divOption = document.createElement(`div`);
             divOption.classList.add(`option`);
@@ -50,13 +98,27 @@ class Quiz {
             input.name = activQuestion.options[activQuestion.correctAnswer];
             input.value = option;
             let label = document.createElement(`label`);
-            label.htmlFor = `choice${index}`;
+            label.htmlFor = `choice-${index}`;
             label.innerText = option;
+
+            form.addEventListener(`submit`, (event) => {
+                event.preventDefault();
+                if(input.checked){
+                    // console.log(input.value);
+                    if(activQuestion.isCorrect(input.value)){
+                        if(!this.clickOnce){
+                            this.updateScore();
+                            this.clickOnce = this.clickOnce + 1;
+                        }
+                    };
+                }
+            });
             divOption.append(input,label);
             divOptions.append(divOption);
         })
         fieldset.append(legend, divOptions, divBtn);
         form.append(fieldset);
+        quizElm .append(form);
     }
 }
 
@@ -84,3 +146,17 @@ data.forEach((ques) => {
     
 })
 competition.createUI();
+
+
+
+nextBtn.addEventListener(`click`, competition.nextQuestion.bind(competition));
+prevBtn.addEventListener(`click`, competition.prevQuestion.bind(competition));
+resultBtn.addEventListener(`click`, () => {
+    alert(`Your score is ${competition.score}`);
+    competition.getAnswers();
+    resetBtn.style.visibility = "visible";
+})
+resetBtn.addEventListener(`click`, () => {
+    competition.restart();
+    resetBtn.style.visibility = "hidden";
+})
